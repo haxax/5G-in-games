1177bb266471a45995aba79a8f6bd22ac684959a
Ebema0 <bybenh@gmail.com>
2024-11-28 19:37:50 +0200

    Some ui And Crystal

--- Unity/Assets/Scripts/WorldObjects/PlayerObject.cs
+++ Unity/Assets/Scripts/WorldObjects/PlayerObject.cs
@@ -1,15 +1,29 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class PlayerObject : MonoBehaviour
 {
     [SerializeField] private FivegPoint my5G;
     [SerializeField] private bool isSimulated = false;
+    private int currentScore;
+    public TMP_Text scoreText;
+
 
     private void Start()
     {
         SetLocation(WorldManager.Instance.CurrentGpsLocation);
         WorldManager.Instance.OnGpsLocationUpdate.AddListener(SetLocation);
+        currentScore = 0;
     }
+
+    private void HandleScore()
+    {
+        scoreText.text = "Score: " + currentScore;
+    }
+
     public void SetLocation(Vector2 ingameLocation)
     {
         if (isSimulated) { return; }
@@ -20,5 +34,13 @@ public class PlayerObject : MonoBehaviour
     {
         transform.position = new Vector3(ingameLocation.x, 0f, ingameLocation.y);
         my5G.SetCircleMaterial(my5G.YellowMat);
+    }
+    void onCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Crystal")
+        {
+            currentScore ++;
+            HandleScore();
+        }
     }
 }
