94e785ff7bddafe222f66fb0b2f50b3a7d02d5ea
mongah1 <hitesh.monga@aalto.fi>
2024-11-28 21:18:49 +0200

    add location parameters

--- Backend/app.py
+++ Backend/app.py
@@ -27,13 +27,7 @@ def retrieve_location():
     # Payload to send to the Nokia API
     payload = {
         "device": {
-            "phoneNumber": "+358311100537",
-            "networkAccessIdentifier": "device@testcsp.net",
-            "ipv4Address": {
-                "publicAddress": "217.140.216.37",
-                "privateAddress": "192.168.32.1",
-                "publicPort": 80
-            },
+            "phoneNumber": "+358311100537"
         },
         "maxAge": 3600
     }
--- Backend/test.py
+++ Backend/test.py
@@ -4,13 +4,7 @@ url = "http://127.0.0.1:5000/api/location"
 
 payload = {
     "device": {
-        "phoneNumber": "+358311100537",
-        "networkAccessIdentifier": "device@testcsp.net",
-        "ipv4Address": {
-            "publicAddress": "217.140.216.37",
-            "privateAddress": "192.168.32.1",
-            "publicPort": 80
-        },
+        "phoneNumber": "+358311100537"
     },
     "maxAge": 3600
 }
